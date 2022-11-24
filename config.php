6f02cedca87dabcb47f983140e94902feef2bc0f
SPRT1 <118957963+SPRT1@users.noreply.github.com>
2022-11-23 23:50:31 -0600

    initial commit

---
+++ config.php
@@ -0,0 +1,619 @@
+<?php
+
+// Meta Information
+// All will be used for embeds primarily.
+
+$title = "Your Title";
+$description = "Your Description";
+$favicon = "";
+$themecolor = "#FFFFFF";
+
+// Header Information
+
+$name = "NisshokuHost";
+$icon = "assets/img/images.jpg";
+$headerTitle = "Bienvenido a NisshokuHost";
+$headerDesc = "Somos una empresa dedicada a poder hostear tus servidores ,Nos encargamos que con un muy poco Dinero puedas tener tus bots o servidores en linea y tambien  tener tus servidores grandes y en buen estado";
+
+// Navbar
+
+$Navbar = [
+    [
+        "Name" => "Discord",
+        "Link" => "https://discord.gg/Fka6UEuBfs"
+    ]
+];
+
+
+
+// Section One
+$sectionOneTitle = "CARACTERÍSTICAS DE NUESTRO HOST";
+$sectionOneSlogan = "";
+
+// Features
+$Features = [
+    [
+        "Icon" => "assets/img/amd_ryzen_9_logo.png",
+        "Title" => "Ryzen 9 3900X y buena latencia"
+    ],
+    [
+        "Icon" => "assets/img/eb5f-ddr4-post-image-3.jpg",
+        "Title" => "Ram DDR4 & Disco SSD NVMe"
+    ],
+    [
+        "Icon" => "assets/img/console.02ecad9.png",
+        "Title" => "Panel de Control y Backups"
+    ],
+    [
+        "Icon" => "assets/img/español-ingles.jpg",
+        "Title" => "Soporte En Español/Ingles"
+    ],
+    [
+        "Icon" => "assets/img/money-bag.png",
+        "Title" => "Planes empiezan apartir de 1$"
+    ],
+    [
+        "Icon" => "assets/img/Free-Hosting.jpg",
+        "Title" => "Contamos con nuestros planes gratuitos"
+    ],
+];
+
+// Section Two
+$sectionTwoTitle = "Productos";
+$sectionTwoSlogan = "Se hace entrega de los servicios en nuestro servidor de discord https://discord.gg/Fka6UEuBfs ";
+
+// Products
+
+$Products = [
+    [
+        "Image" => "assets/img/minecraft.png",
+        "Title" => "Minecraft Plan Principiante",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        2 Nucleos /
+        1 backups /
+        
+        Host: Java,Bedrock,Proxy /
+        Cantidad de Jugadores: ilimitada /
+        Mods/Plugins (en caso de querer): 2 /
+        
+        Precio: 1$",
+        "Category" => "Minecraft",
+        "SellixID" => "637ef1607488a"
+    ],
+    [
+        "Image" => "assets/img/minecraft.png",
+        "Title" => "Minecraft Plan Basico",
+        "Description" => "2gb de RAM /
+        4gb de espacio en disco /
+        2 Nucleos /
+        2 backups /
+        
+        Host: Java,Bedrock,Proxy /
+        Cantidad de Jugadores: ilimitada /
+        Mods/Plugins (en caso de querer): 12 /
+        
+        Precio: 3$ x mes",
+        "Category" => "Minecraft",
+        "SellixID" => "637ef5bd6a9e5"
+    ],
+    [
+        "Image" => "assets/img/minecraft.png",
+        "Title" => "Minecraft Plan Intermedio",
+        "Description" => "4gb de RAM /
+        6gb de espacio en disco /
+        2 Nucleos /
+        2 backups /
+        
+        Host: Java,Bedrock,Proxy /
+        Cantidad de Jugadores: ilimitada /
+        Mods/Plugins (en caso de querer): 36 /
+        
+        Precio: 5$ x mes",
+        "Category" => "Minecraft",
+        "SellixID" => "637ef611abfbf"
+    ],
+    [
+        "Image" => "assets/img/minecraft.png",
+        "Title" => "Minecraft Plan Avanzado",
+        "Description" => "6gb de RAM /
+        8gb de espacio en disco /
+        3 Nucleos /
+        4 backups /
+        
+        Host: Java,Bedrock,Proxy /
+        Cantidad de Jugadores: ilimitada /
+        Mods/Plugins (en caso de querer): 55 /
+        
+        Precio: 8$ x mes",
+        "Category" => "Minecraft",
+        "SellixID" => "637ef64a0b308"
+    ],
+    [
+        "Image" => "assets/img/minecraft.png",
+        "Title" => "Minecraft Plan Premium",
+        "Description" => "10gb de RAM /
+        15gb de espacio en disco /
+        6 Nucleos /
+        10 backups /
+        
+        Host: Java,Bedrock,Proxy /
+        Cantidad de Jugadores: ilimitada /
+        Mods/Plugins (en caso de querer): 80 /
+        
+        Precio: 14$ x mes",
+        "Category" => "Minecraft",
+        "SellixID" => "637ef68ba4b5c"
+    ],
+ //Discord
+    [
+        "Image" => "assets/img/discordbothost.png",
+        "Title" => "Plan Basico",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        1 backups /
+        
+        Plan recomendado para bots para uso de 1 servidor /
+        » Soporte a sus problemas /
+        » Host 24/7 /
+        » Instalacion Gratuita De Su Codigo /
+        
+        Precio: 2$ x mes",
+        "Category" => "Discord Bot Host",
+        "SellixID" => "637ef88023f23"
+    ],
+    [
+        "Image" => "assets/img/discordbothost.png",
+        "Title" => "Plan Intermedio ",
+        "Description" => "3gb de RAM /
+        6gb de espacio en disco /
+        2 Nucleos /
+        3 backups /
+        
+        Plan recomendado para bots con +16 servers /
+        » Soporte a sus problemas /
+        » Host 24/7 /
+        » Instalacion Gratuita De Su Codigo /
+        
+        Precio: 4$ x mes",
+        "Category" => "Discord Bot Host",
+        "SellixID" => "637ef8d65942b"
+    ],
+    [
+        "Image" => "assets/img/discordbothost.png",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        12gb de espacio en disco /
+        3 Nucleos /
+        3 backups /
+        
+        Plan recomendado para bots con +50 servers  /
+        » Soporte a sus problemas /
+        » Host 24/7 /
+        » Instalacion Gratuita De Su Codigo /
+        
+        Precio: 8$ x mes",
+        "Category" => "Discord Bot Host",
+        "SellixID" => "637ef90c3fffe"
+    ],
+    [
+        "Image" => "assets/img/discordbothost.png",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        16gb de espacio en disco /
+        4 Nucleos /
+        6 backups /
+        
+        Plan recomendado Plan recomendado para bots con +100 servers /
+        » Soporte a sus problemas /
+        » Host 24/7 /
+        » Instalacion Gratuita De Su Codigo /
+        
+        Precio: 14$ x mes",
+        "Category" => "Discord Bot Host",
+        "SellixID" => "637ef935338a2"
+    ],
+    //gen bot
+    [
+        "Image" => "assets/img/genbot.jpg",
+        "Title" => "Plan Base",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        1 backups /
+        
+        
+        » Soporte a sus problemas /
+        » Host 24/7 /
+        -Nosotros proporcionamos el codigo 🇺🇸 o 🇪🇸 /
+        -Bot de uso de 1 servido /
+        
+        Precio: 2$ x mes",
+        "Category" => "Discord Bot Gen",
+        "SellixID" => "637ef96ab9a8a"
+    ],
+    //GTA5
+    [
+        "Image" => "assets/img/938899.jpg",
+        "Title" => "Plan Intermedio",
+        "Description" => "4gb de RAM /
+        10gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 4$  x mes",
+        "Category" => "GTA5",
+        "SellixID" => "637ef9bf889b4"
+    ],
+    [
+        "Image" => "assets/img/938899.jpg",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        20gb de espacio en disco /
+        4 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 8$  x mes",
+        "Category" => "GTA5",
+        "SellixID" => "637efa0191ad3"
+    ],
+    [
+        "Image" => "assets/img/938899.jpg",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        30gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 15$  x mes",
+        "Category" => "GTA5",
+        "SellixID" => "637efa2d47603"
+    ],
+    //Ark
+    [
+        "Image" => "assets/img/ark.jpg",
+        "Title" => "Plan Intermedio",
+        "Description" => "3gb de RAM /
+        10gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 4$  x mes",
+        "Category" => "ARK",
+        "SellixID" => "637efa94ebe6b"
+    ],
+    [
+        "Image" => "assets/img/ark.jpg",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        15gb de espacio en disco /
+        4 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 8$  x mes",
+        "Category" => "ARK",
+        "SellixID" => "637efae145134"
+    ],
+    [
+        "Image" => "assets/img/ark.jpg",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        25gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        Precio: 14$  x mes",
+        "Category" => "ARK",
+        "SellixID" => "637efb12ba8da"
+    ],
+    //voice servers
+    [
+        "Image" => "assets/img/voiceservers.png",
+        "Title" => "Plan Basico",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Plan por separado mismo precio para
+        Lavalink,Mumble,TeamSpeak,TeamSpeak3
+        
+        Precio: 1$  x mes",
+        "Category" => "Voice Servers",
+        "SellixID" => "637efb3e0e181"
+    ],
+    [
+        "Image" => "assets/img/voiceservers.png",
+        "Title" => "Plan Intermedio",
+        "Description" => "3gb de RAM /
+        5gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Plan por separado mismo precio para
+        Lavalink,Mumble,TeamSpeak,TeamSpeak3
+        
+        Precio: 4$  x mes",
+        "Category" => "Voice Servers",
+        "SellixID" => "637eff3219e0e"
+    ],
+    [
+            "Image" => "assets/img/voiceservers.png",
+            "Title" => "Plan Avanzado",
+            "Description" => "6gb de RAM /
+            12gb de espacio en disco /
+            4 Nucleos /
+            
+            Servidor 24/7 /
+            Soporte siempre activo /
+            Plan por separado mismo precio para
+            Lavalink,Mumble,TeamSpeak,TeamSpeak3
+            
+            Precio: 8$  x mes",
+            "Category" => "Voice Servers",
+            "SellixID" => "637eff5958b64"
+    ],
+    [
+        "Image" => "assets/img/voiceservers.png",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        20gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Plan por separado mismo precio para
+        Lavalink,Mumble,TeamSpeak,TeamSpeak3
+        
+        Precio: 14$  x mes",
+        "Category" => "Voice Servers",
+        "SellixID" => "637eff7e097e0"
+    ],
+    //CSGO
+    [
+        "Image" => "assets/img/csgo.jpg",
+        "Title" => "Plan Basico",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 1$  x mes",
+        "Category" => "CSGO",
+        "SellixID" => "637effd59f637"
+    ],
+    [
+        "Image" => "assets/img/csgo.jpg",
+        "Title" => "Plan Intermedio",
+        "Description" => "3gb de RAM /
+        5gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 4$  x mes",
+        "Category" => "CSGO",
+        "SellixID" => "637f0000c75d7"
+    ],
+    [
+        "Image" => "assets/img/csgo.jpg",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        12gb de espacio en disco /
+        4 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 8$  x mes",
+        "Category" => "CSGO",
+        "SellixID" => "637f0024f334b"
+    ],
+    [
+        "Image" => "assets/img/csgo.jpg",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        20gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 14$  x mes",
+        "Category" => "CSGO",
+        "SellixID" => "637f0046676df"
+    ],
+    //aplicaciones
+    [
+        "Image" => "assets/img/apps.png",
+        "Title" => "Plan Basico",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 1$  x mes",
+        "Category" => "Aplicaciones",
+        "SellixID" => "637f00e338a54"
+    ],
+    [
+        "Image" => "assets/img/apps.png",
+        "Title" => "Plan Intermedio",
+        "Description" => "3gb de RAM /
+        5gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 4$  x mes",
+        "Category" => "Aplicaciones",
+        "SellixID" => "637f011853b99"
+    ],
+    [
+        "Image" => "assets/img/apps.png",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        12gb de espacio en disco /
+        4 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 8$  x mes",
+        "Category" => "Aplicaciones",
+        "SellixID" => "637f013ee9a89"
+    ],
+    [
+        "Image" => "assets/img/apps.png",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        20gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        
+        
+        Precio: 14$  x mes",
+        "Category" => "Aplicaciones",
+        "SellixID" => "637f016da06ae"
+    ],
+    //steam
+    [
+        "Image" => "assets/img/stea,.jpg",
+        "Title" => "Plan Basico",
+        "Description" => "1gb de RAM /
+        3gb de espacio en disco /
+        1 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Consultar disponibilidad del juego en discord /
+        
+        
+        Precio: 1$  x mes",
+        "Category" => "Juegos Steam",
+        "SellixID" => "637f019455ca7"
+    ],
+    [
+        "Image" => "assets/img/stea,.jpg",
+        "Title" => "Plan Intermedio",
+        "Description" => "3gb de RAM /
+        5gb de espacio en disco /
+        2 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Consultar disponibilidad del juego en discord /
+        
+        
+        Precio: 4$  x mes",
+        "Category" => "Juegos Steam",
+        "SellixID" => "637f01dc85633"
+    ],
+    [
+        "Image" => "assets/img/stea,.jpg",
+        "Title" => "Plan Avanzado",
+        "Description" => "6gb de RAM /
+        12gb de espacio en disco /
+        4 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Consultar disponibilidad del juego en discord /
+        
+        
+        Precio: 8$  x mes",
+        "Category" => "Juegos Steam",
+        "SellixID" => "637f02073fce4"
+    ],
+    [
+        "Image" => "assets/img/stea,.jpg",
+        "Title" => "Plan Premium",
+        "Description" => "10gb de RAM /
+        20gb de espacio en disco /
+        5 Nucleos /
+        
+        Servidor 24/7 /
+        Soporte siempre activo /
+        Consultar disponibilidad del juego en discord /
+        
+        Precio: 14$  x mes",
+        "Category" => "Juegos Steam",
+        "SellixID" => "37f0231effe9"
+    ],
+    //Complementos
+    [
+        "Image" => "assets/img/best-web-hosting.jpg",
+        "Title" => "RAM",
+        "Description" => "1gb de ram = 1$ /
+        500mb de ram = 0,50$ /
+        300mb de ram = 0,30$ /
+
+        Precio se agrega Extra a tu plan",
+        "Category" => "Complementos",
+        "SellixID" => "637f02622ff99"
+    ],
+    [
+        "Image" => "assets/img/best-web-hosting.jpg",
+        "Title" => "STORAGE",
+        "Description" => "1gb de disco = 0,50$ /
+        500mb de disco = 0,25$ /
+
+        Precio se agrega Extra a tu plan",
+        "Category" => "Complementos",
+        "SellixID" => "637f031ecdef4"
+    ],
+    [
+        "Image" => "assets/img/best-web-hosting.jpg",
+        "Title" => "CPU",
+        "Description" => "
+        2 Nucleo de CPU = 3$ /
+
+        Precio se agrega Extra a tu plan",
+        "Category" => "Complementos",
+        "SellixID" => "637f03ac57050"
+    ],
+    
+
+    
+
+
+];
+
+
+
+
+
+
+    
+    
+    
